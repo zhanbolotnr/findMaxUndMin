078e3a910ebda4d1bdb720ff5cd0639970df1b19
Zhanbolot <zhanbolotnr@gmail.com>
2022-03-23 23:49:21 +0600

    findMaxundMin

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MinUndMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number of elements? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("1 st element: ");
+            int max = int.Parse(Console.ReadLine());
+            int min = max;
+            for (int i = 2; i <= n; ++i)
+            {
+                Console.Write($"{i} element? ");
+                int cur = int.Parse(Console.ReadLine());
+                if (cur > max)
+                {
+                    max = cur;
+                }
+                else if (cur < min)
+                {
+                    min = cur;
+                }
+            }
+            Console.WriteLine($"Maximum: {max}. \nMinimum: {min}.");
+        }
+    }
+}
